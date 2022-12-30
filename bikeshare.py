16de99d79ef0a304d0a299424759d5e4bd71b5b6
Shaden Abdulaziz Alabdulkarim <shaden.abdulaziz1@gmail.com>
2022-12-30 19:22:20 +0300

    refactor:Adding print statement

--- bikeshare.py
+++ bikeshare.py
@@ -95,7 +95,7 @@ def time_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-
+ print('\nEDITING\n')
 
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
